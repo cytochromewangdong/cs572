4d672cbaa57a2d4e51c827227fa26d1331740bc0
dongwang <dongwang@wangdongMacBook-Pro.local>
2019-04-05 07:45:51 -0500

    Added the subscribe of the exception event

--- homework_04/exercise2/fileWorker.js
+++ homework_04/exercise2/fileWorker.js
@@ -2,17 +2,15 @@ const fs = require("fs");
 process.on("message",e=>{
     const handler = {
         start(data) {
-            try{
-                const fstream  = fs.createReadStream(data);
-                fstream.on('data',(d)=>{
-                    process.send({action:'data', data: d.toString("base64")});
-                }).on('end', ()=>{
-                    process.send({action:'end'});
-                });
-            } catch(e)
-            {
-                process.send({action:'error',error:JSON.stringify(e)}); 
-            }
+            const fstream  = fs.createReadStream(data);
+            fstream.on('data',(d)=>{
+                process.send({action:'data', data: d.toString("base64")});
+            }).on('end', ()=>{
+                process.send({action:'end'});
+            }).on('error',(err)=>{
+                // Finally, I found the exception is handled by the event.
+                process.send({action:'error',error:JSON.stringify(err)}); 
+            });
         },
         end(data){
             process.exit(0);
