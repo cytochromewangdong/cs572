0e3bc54c386a86f8a56fbb7416d2128a9fbeb558
dongwang <dongwang@wangdongMacBook-Pro.local>
2019-04-06 22:05:44 -0500

    Added the Json check(simple mode)

--- homework_06/exercise01/JsonCheckMiddleWare.js
+++ homework_06/exercise01/JsonCheckMiddleWare.js
@@ -1,33 +1,26 @@
+const createError = require('http-errors');
 const jsonChecker = function( req, res, next ) {
   let data = '';
   req.on('data', (chunk)=>{
       data  += chunk;
   })
-  req.on('end', ()=>{
+  req.on('end', function() {
       req.rawBody = data;
       if(data)
       {
+        console.log( 'on end: ', data )
         if(data.indexOf('{') >=0){
             try{
                 req.body = JSON.parse(data);
             } catch(e)
             {
-                next(e);
+                return next(createError(400));
             }
         } else {
-            next("Invalid json format!");
+            return next(createError(400));
         }
-
       } 
       next();
-  });
-  req.on('end', function() {
-    req.rawBody = data;
-    console.log( 'on end: ', data )
-    if (data && data.indexOf('{') > -1 ) {
-      req.body = JSON.parse(data);
-    }
-    next();
   });
 }
 module.exports = jsonChecker;
--- homework_06/exercise01/app.js
+++ homework_06/exercise01/app.js
@@ -4,20 +4,21 @@ const path = require('path');
 const morgan = require('morgan');
 const fs = require("fs");
 const cors = require("cors");
-// const jsonChecker = require("JsonCheckMiddleWare");
+const helmet = require("helmet");
+const jsonChecker = require("./JsonCheckMiddleWare");
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
 
 var usersRouter = require('./routes/grades');
 
 var app = express();
-
-app.use(cors());
+app.use(helmet());
+app.use(cors())
 app.use(morgan('combined', { stream: accessLogStream }))
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(jsonChecker);
+// app.use(express.json());
 
 app.use('/grades', usersRouter);
-
+app.use(cors());
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
