aede4a7f65e4d89ceb0dd8ed7e7432737633c1d1
dongwang <dongwang@wangdongMacBook-Pro.local>
2019-04-07 10:28:41 -0500

    Added zip/inflate and charset support

--- homework_06/exercise01/JsonCheckMiddleWare.js
+++ homework_06/exercise01/JsonCheckMiddleWare.js
@@ -1,14 +1,42 @@
 const createError = require('http-errors');
+const zlib = require('zlib')
+const contentType = require('content-type')
 const jsonChecker = function( req, res, next ) {
-  let data = '';
-  req.on('data', (chunk)=>{
-      data  += chunk;
+  var length = req.headers['content-length'];
+  var encoding = (req.headers['content-encoding']||'plain').toLowerCase()
+  switch (encoding) {
+    case 'deflate':
+      stream = zlib.createInflate()
+      req.pipe(stream)
+      break
+    case 'gzip':
+      stream = zlib.createGunzip()
+      req.pipe(stream)
+      break
+    case 'plain':
+      stream = req
+      stream.length = length
+      break
+    default:
+        next(createError(415, `unsupported content encoding ${encoding}`));
+  }
+  let charset = 'utf8';
+  try{
+    charset = contentType.parse(req).parameters.charset.toLowerCase();
+  } catch(e)
+  {
+    
+  }
+ 
+  let buffer = [];
+  stream.on('data', (chunk)=>{
+    buffer.push(chunk);
   })
-  req.on('end', function() {
+  stream.on('end', function() {
+      var data  = Buffer.concat(buffer).toString("utf8");
       req.rawBody = data;
       if(data)
       {
-        // console.log( 'on end: ', data )
         if(data.indexOf('{') >=0){
             try{
                 req.body = JSON.parse(data);
